2b1cca87eed119f269a2bb0e722095f6996765d2
Dale Winger <dalewinger@gmail.com>
2018-09-07 15:16:59 -0700

    Adds apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
